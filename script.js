e200c5e6de907e2463c950d7467572ecb57adbaf
Philipp Heitmann <philipp.heitmann94@gmail.com>
2021-08-20 11:20:46 +0200

    ss

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ const predict = () => {
         query.push(`${param}=${data[param]}`)
       })
       const querystring = query.join('&')
-      const url = `${taxiFareApiUrl}?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2`
+      const url = `${taxiFareApiUrl}?${querystring}`
       fetch(url, {
         method: 'GET',
         headers: {
